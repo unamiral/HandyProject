2beff4c28395483d2fd61d2933d28560eb3e6350
amirkasra <amirkasraesmaeilian@gmail.com>
2021-05-08 15:42:26 +0430

    add prime module

--- IntegerFactorization.py
+++ IntegerFactorization.py
@@ -1,10 +1,15 @@
+import prime
+
 def integerFactorization(num):
     primeNums = (2, 3, 5, 7)
     result = []  # save our numbers
-    while num != 1:
-        for i in primeNums:
-            if num % i == 0:
-                num /= i
-                result.append(i)  # add it to result
-                break
+    if prime.prime(num) == False:    
+        while num != 1:
+            for i in primeNums:
+                if num % i == 0:
+                    num /= i
+                    result.append(i)  # add it to result
+                    break
+    else:
+        return "It's a prime number."
     return result  # our result
