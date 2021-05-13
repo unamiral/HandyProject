6812a07c7e2162c71aa2fb601f4484e47bfe633f
amirkasra <amirkasraesmaeilian@gmail.com>
2021-05-13 15:41:51 +0430

    add power solution for better result

--- prime.py
+++ prime.py
@@ -1,8 +1,11 @@
 def prime(num):
+    x = num ** 0.5
     if num != 1:
         if num != 2: # bug in range
             for i in range(2, num):
-                if num % i != 0: # check divisibility to all number before var num.
+                if x * x == num :
+                    return False
+                elif num % i != 0: # check divisibility to all number before var num.
                     return True  # Divisibility should be != 0 to be a prime number.
                 else:
                     return False 
